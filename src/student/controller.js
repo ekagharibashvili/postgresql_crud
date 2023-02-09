9dc0a5d3ecf2be7b42ba0f0b6a5f78a157b15597
Eka Garibashvili <EkaGaribashvili98@gmail.com>
2023-02-10 02:37:30 +0400

    add updatestudent controller

--- src/student/controller.js
+++ src/student/controller.js
@@ -30,9 +30,40 @@ const addStudent = (req, res) => {
       (error, results) => {
         if (error) throw error;
         res.status(201).send("Student created successfully!");
-        console.log("student created")
+        console.log("student created");
       }
     );
+  });
+};
+
+const deleteStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    const noStudentFound = !results.rows.length;
+    if (noStudentFound) {
+      res.send("Student does not exist in the database.");
+    }
+    pool.query(queries.deleteStudent, [id], (error, results) => {
+      if (error) throw error;
+      res.status(204).send("Student deleted successfully!");
+      console.log("student deleted");
+    });
+  });
+};
+
+const updateStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    const noStudentFound = !results.rows.length;
+    if (noStudentFound) {
+      res.send("Student does not exist in the database.");
+    }
+
+    pool.query(queries.updateStudent, [name, id], (error, results) => {
+      if (error) throw error;
+      res.status(200).send("Student updated successfully");
+    });
   });
 };
 
@@ -40,4 +71,6 @@ module.exports = {
   getStudents,
   getStudentById,
   addStudent,
+  deleteStudent,
+  updateStudent,
 };
