3de235d902d99c73771f59e773ac9c28f56cd64f
Eka Garibashvili <EkaGaribashvili98@gmail.com>
2023-02-09 05:49:15 +0400

    add controllers and routes

--- server.js
+++ server.js
@@ -1,67 +1,15 @@
 const express = require("express");
-const morgan = require("morgan");
-require("dotenv").config();
-
-
+const studentRoutes = require("./src/student/routes");
 const app = express();
-
 const port = 3000;
 
-app.use(morgan("common"));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        
-        <form action = "/info/get" method="GET"> 
-            <input type="submit" value="GET">
-        </form>
-        <form action = "/info/add" method="POST"> 
-            <label for="add">ADD:</label>
-            <input type="text" name="add" id="add">
-            <input type="submit" value="ADD">
-        </form>
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-        <form action = "/info/delete" method="POST"> 
-            <label for="delete">DELETE:</label>
-            <input type="text" name="delete" id="delete">
-            <input type="submit" value="DELETE">
-        </form>
-
-        <form action = "/info/update" method="POST"> 
-            <label for="oldValue">OLD VALUE:</label>
-            <input type="text" name="oldValue" id="oldValue">
-            <label for="newValue">NEW VALUE:</label>
-            <input type="text" name="newValue" id="newValue">
-            <input type="submit" value="UPDATE">
-        </form>
-
-    </body>
-    </html>
-    `)
-})
-
-app.get('/info/get',(req, res) => {
-    try {
-        pool.connect(async(error, client, release) => {
-            let resp = await client.query(`SELECT * FROM test`);
-            release();
-            res.json(resp.rows)
-        })
-
-    } catch (err) {
-        console.error(err)
-    }
-})
+app.use("/api/v1/students", studentRoutes);
 
 app.listen(port, () => {
   console.log(`server starting on port ${port}`);
---
+++ src/student/controller.js
@@ -0,0 +1,21 @@
+const pool = require("../../db");
+const queries = require("./queries");
+
+const getStudents = (req, res) => {
+  pool.query(queries.getStudents, (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
+
+const getStudentById = (req, res) => {
+    pool.query(queries.getStudentById, (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    })
+}
+
+module.exports = {
+  getStudents,
+  getStudentById,
+};
